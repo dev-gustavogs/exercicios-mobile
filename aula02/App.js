71bb65de8ff7106d7c5851f9dcfd882b62c3370f
Gustavo Santos <gustavogomes.bsb68@gmail.com>
2025-10-09 13:09:32 -0300

    aula03

--- aula02/App.js
+++ aula02/App.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Layout_Responsivo from './src/components/Layout_Responsivo'
+import CartaoPerfil from './src/components/CartaoPerfil'
+import CartaoApresentacao from './src/components/CartaoApresentacao'
 
 const App = () => {
   return (
     <View style={{flex:1}}>
-      <Layout_Responsivo/>
+      <CartaoApresentacao/>
     </View>
   )
 }
---
+++ aula03/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import LoginScreen from './src/screems/LoginScreens'
+
+const App = () => {
+  return (
+    <View>
+      <LoginScreen/>
+    </View>
+  )
+}
+
+export default App
